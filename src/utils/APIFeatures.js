1bad1be5dadd642e8bedc6e01d0704a4ac588c6e
Melchor16 <erick.vimel@hotmail.com>
2025-03-12 17:35:28 -0600

    queda lista la función filter() del song controller

--- src/utils/APIFeatures.js
+++ src/utils/APIFeatures.js
@@ -8,8 +8,39 @@ class APIFeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
+    //excluding this fields bc they are used later
     const excludeFields = ["page", "sort", "limit", "fields"];
     excludeFields.forEach((el) => delete queryObj[el]);
+
+    //maping operators to then convert the req.paramas to valid sequelize operators
+    const operatorsMap = {
+      gte: Op.gte,
+      gt: Op.gt,
+      lte: Op.lte,
+      lt: Op.lt,
+    };
+
+    //creating where object
+    const where = {};
+
+    // mapping columns
+    for (const [key, value] of Object.entries(queryObj)) {
+      if (typeof value === "object" && value !== null) {
+        const conditions = {};
+        // mapping values
+        for (const [op, val] of Object.entries(value)) {
+          if (operatorsMap[op]) {
+            conditions[operatorsMap[op]] = Number(val);
+          }
+        }
+        where[key] = conditions;
+      } else {
+        where[key] = value;
+      }
+    }
+
+    this.queryOptions.where = where;
+
     return this;
   }
 
